b2760e6c8be0e0c0ced788d2ba6ba7fde44ad92e
Tamy <tamy.vivas@gmail.com>
2020-12-30 13:52:29 -0600

    feat: custom hook and helper

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,36 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
-    const [images, setImages] = useState([]);
-
-    useEffect(() => {
-        getGifs();
-    }, []);
-
-    const getGifs = async () => {
-        const url =
-            'https://api.giphy.com/v1/gifs/search?q=batman&limit=10&api_key=TW4E4Pk0HOYgDGCRyq7ijMMto29HpxFd';
-        const resp = await fetch(url);
-        const { data } = await resp.json();
-
-        const gifs = data.map((img) => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url,
-            };
-        });
-        console.log(gifs);
-        setImages(gifs);
-    };
+    const { data: images, loading } = useFetchGifs(category);
 
     return (
-        <div>
-            <h3>{category}</h3>
-            {images.map((img) => (
-                <GifGridItem key={img.id} {...img} />
-            ))}
-        </div>
+        <>
+            <h3 className="animate__animated animate__fadeIn animate__faster">
+                {category}
+            </h3>
+            {loading && (
+                <p className="animate__animated animate__flash">Loading...</p>
+            )}
+            <div className="card-grid">
+                {images.map((img) => (
+                    <GifGridItem key={img.id} {...img} />
+                ))}
+            </div>
+        </>
     );
 };
---
+++ 04-gif-expert-app/src/helpers/getGifs.js
@@ -0,0 +1,18 @@
+export const getGifs = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
+        category
+    )}&limit=10&api_key=TW4E4Pk0HOYgDGCRyq7ijMMto29HpxFd`;
+
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifs = data.map((img) => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url,
+        };
+    });
+
+    return gifs;
+};
