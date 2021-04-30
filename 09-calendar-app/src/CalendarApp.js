c86bb62007e14aba18a4d61c9eb7ca86ade9591f
Tamy <tamy.vivas@gmail.com>
2021-04-30 13:43:20 -0600

    09-calendar-app initial commit

---
+++ 09-calendar-app/src/CalendarApp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const CalendarApp = () => {
+    return (
+        <div>
+            <h1>Hola Calendar</h1>
+        </div>
+    );
+};
---
+++ 09-calendar-app/src/router/AppRouter.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Redirect,
+    Route,
+    Switch,
+} from 'react-router-dom';
+import { LoginScreen } from '../components/auth/LoginScreen';
+import { CalendarScreen } from '../components/calendar/CalendarScreen';
+{
+    /**
+     * exact /login -LoginScreen
+     * exact / -CalendarScreen
+     */
+}
+export const AppRouter = () => {
+    return (
+        <Router>
+            <div>
+                <Switch>
+                    <Route exact path="/login">
+                        <LoginScreen />
+                    </Route>
+                    <Route path="/">
+                        <CalendarScreen />
+                    </Route>
+                    <Redirect to="/" />
+                </Switch>
+            </div>
+        </Router>
+    );
+};
