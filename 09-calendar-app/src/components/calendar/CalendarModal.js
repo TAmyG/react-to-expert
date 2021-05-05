fa7e4092f02d1054c27ccd020853d7db970b78c7
Tamy <tamy.vivas@gmail.com>
2021-05-04 18:14:28 -0600

    frontend calendar-app completed

--- 09-calendar-app/src/components/calendar/CalendarModal.js
+++ 09-calendar-app/src/components/calendar/CalendarModal.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useState } from 'react';
+
+import moment from 'moment';
 import Modal from 'react-modal';
 import DateTimePicker from 'react-datetime-picker';
 import Swal from 'sweetalert2';
 
-import moment from 'moment';
 import { uiCloseModal } from '../../actions/ui';
+import {
+    eventAddNew,
+    eventClearActiveEvent,
+    eventUpdated,
+} from '../../actions/events';
 
 const customStyles = {
     content: {
@@ -18,26 +23,38 @@ const customStyles = {
         transform: 'translate(-50%, -50%)',
     },
 };
-
 Modal.setAppElement('#root');
-const now = moment().minutes(0).seconds(0).add(1, 'hours');
-const nowplus1 = now.clone().add(1, 'hours');
+
+const now = moment().minutes(0).seconds(0).add(1, 'hours'); // 3:00:00
+const nowPlus1 = now.clone().add(1, 'hours');
+
+const initEvent = {
+    title: '',
+    notes: '',
+    start: now.toDate(),
+    end: nowPlus1.toDate(),
+};
 
 export const CalendarModal = () => {
-    const dispatch = useDispatch();
     const { modalOpen } = useSelector((state) => state.ui);
-    const [startDate, setStartDate] = useState(now.toDate());
-    const [endDate, setEndDate] = useState(nowplus1.toDate());
+    const { activeEvent } = useSelector((state) => state.calendar);
+    const dispatch = useDispatch();
+
+    const [dateStart, setDateStart] = useState(now.toDate());
+    const [dateEnd, setDateEnd] = useState(nowPlus1.toDate());
     const [titleValid, setTitleValid] = useState(true);
 
-    const [formValues, setFormValues] = useState({
-        title: 'Evento',
-        notes: '',
-        start: now.date(),
-        end: nowplus1.date(),
-    });
+    const [formValues, setFormValues] = useState(initEvent);
 
     const { notes, title, start, end } = formValues;
+
+    useEffect(() => {
+        if (activeEvent) {
+            setFormValues(activeEvent);
+        } else {
+            setFormValues(initEvent);
+        }
+    }, [activeEvent, setFormValues]);
 
     const handleInputChange = ({ target }) => {
         setFormValues({
@@ -46,16 +63,23 @@ export const CalendarModal = () => {
         });
     };
 
-    const handleStartChange = (e) => {
-        setStartDate(e);
+    const closeModal = () => {
+        // TODO: cerrar el modal
+        dispatch(uiCloseModal());
+        dispatch(eventClearActiveEvent());
+        setFormValues(initEvent);
+    };
+
+    const handleStartDateChange = (e) => {
+        setDateStart(e);
         setFormValues({
             ...formValues,
             start: e,
         });
     };
 
-    const handleEndChange = (e) => {
-        setEndDate(e);
+    const handleEndDateChange = (e) => {
+        setDateEnd(e);
         setFormValues({
             ...formValues,
             end: e,
@@ -64,51 +88,58 @@ export const CalendarModal = () => {
 
     const handleSubmitForm = (e) => {
         e.preventDefault();
+
         const momentStart = moment(start);
         const momentEnd = moment(end);
 
         if (momentStart.isSameOrAfter(momentEnd)) {
-            Swal.fire(
+            return Swal.fire(
                 'Error',
-                'End date needs to be higher than Start date',
+                'La fecha fin debe de ser mayor a la fecha de inicio',
                 'error'
             );
-            return;
         }
 
         if (title.trim().length < 2) {
-            setTitleValid(false);
-            return;
+            return setTitleValid(false);
         }
 
-        //TODO: consume API
+        if (activeEvent) {
+            dispatch(eventUpdated(formValues));
+        } else {
+            dispatch(
+                eventAddNew({
+                    ...formValues,
+                    id: new Date().getTime(),
+                    user: {
+                        _id: '123',
+                        name: 'Fernando',
+                    },
+                })
+            );
+        }
 
         setTitleValid(true);
         closeModal();
     };
 
-    const closeModal = () => {
-        dispatch(uiCloseModal());
-    };
-
     return (
         <Modal
             isOpen={modalOpen}
-            // onAfterOpen={afterOpenModal}
             onRequestClose={closeModal}
             style={customStyles}
             closeTimeoutMS={200}
             className="modal"
             overlayClassName="modal-fondo"
         >
-            <h1> Nuevo evento </h1>
+            <h1> {activeEvent ? 'Editar evento' : 'Nuevo evento'} </h1>
             <hr />
             <form className="container" onSubmit={handleSubmitForm}>
                 <div className="form-group">
                     <label>Fecha y hora inicio</label>
                     <DateTimePicker
-                        onChange={handleStartChange}
-                        value={startDate}
+                        onChange={handleStartDateChange}
+                        value={dateStart}
                         className="form-control"
                     />
                 </div>
@@ -116,9 +147,9 @@ export const CalendarModal = () => {
                 <div className="form-group">
                     <label>Fecha y hora fin</label>
                     <DateTimePicker
-                        onChange={handleEndChange}
-                        value={endDate}
-                        minDate={startDate}
+                        onChange={handleEndDateChange}
+                        value={dateEnd}
+                        minDate={dateStart}
                         className="form-control"
                     />
                 </div>
@@ -130,7 +161,7 @@ export const CalendarModal = () => {
                         type="text"
                         className={`form-control ${
                             !titleValid && 'is-invalid'
-                        }`}
+                        } `}
                         placeholder="Título del evento"
                         name="title"
                         autoComplete="off"
--- 09-calendar-app/src/components/calendar/CalendarScreen.js
+++ 09-calendar-app/src/components/calendar/CalendarScreen.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import moment from 'moment';
 
 import 'react-big-calendar/lib/css/react-big-calendar.css';
@@ -11,24 +11,29 @@ import { messages } from '../../helpers/calendar-messages-es';
 import { CalendarEvent } from './CalendarEvent';
 import { CalendarModal } from './CalendarModal';
 import { uiOpenModal } from '../../actions/ui';
+import { eventClearActiveEvent, eventSetActive } from '../../actions/events';
+import { AddNewFab } from '../ui/AddNewFab';
+import { DeleteEventFab } from '../ui/DeleteEventFab';
 
 moment.locale('es');
 const localizer = momentLocalizer(moment); // or globalizeLocalizer
-const events = [
-    {
-        title: 'Boss birthday',
-        start: moment().toDate(),
-        end: moment().add(2, 'hours').toDate(),
-        bgcolor: '#fafafa',
-        notes: 'buy cake',
-        user: {
-            _id: 1234,
-            name: 'Tamy',
-        },
-    },
-];
+// TODO: read from the store all events
+// const events = [
+//     {
+//         title: 'Boss birthday',
+//         start: moment().toDate(),
+//         end: moment().add(2, 'hours').toDate(),
+//         bgcolor: '#fafafa',
+//         notes: 'buy cake',
+//         user: {
+//             _id: 1234,
+//             name: 'Tamy',
+//         },
+//     },
+// ];
 export const CalendarScreen = () => {
     const dispatch = useDispatch();
+    const { events, activeEvent } = useSelector((state) => state.calendar);
 
     const [lastView, setLastView] = useState(
         localStorage.getItem('lastView') || 'month'
@@ -39,12 +44,17 @@ export const CalendarScreen = () => {
     };
 
     const onSelectEvent = (e) => {
-        console.log(e);
+        dispatch(eventSetActive(e));
     };
 
     const onViewChange = (e) => {
         setLastView(e);
         localStorage.setItem('lastView', e);
+    };
+
+    const onSelectSlot = (e) => {
+        //dispatch(uiOpenModal());
+        dispatch(eventClearActiveEvent());
     };
 
     const eventStyleGetter = (event, start, end, isSelected) => {
@@ -73,11 +83,16 @@ export const CalendarScreen = () => {
                 onDoubleClickEvent={onDoubleClick}
                 onSelectEvent={onSelectEvent}
                 onView={onViewChange}
+                onSelectSlot={onSelectSlot}
+                selectable={true}
                 view={lastView}
                 components={{
                     event: CalendarEvent, //Add custom component to events
                 }}
             />
+
+            <AddNewFab />
+            {activeEvent && <DeleteEventFab />}
 
             <CalendarModal />
         </div>
