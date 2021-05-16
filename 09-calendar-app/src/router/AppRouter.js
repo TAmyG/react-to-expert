98b3732b83a6f55f61faa4d1ec2c923a8d4d3824
Tamy <tamy.vivas@gmail.com>
2021-05-16 12:07:48 -0600

    auth functions finished on frontend

--- 09-calendar-app/src/router/AppRouter.js
+++ 09-calendar-app/src/router/AppRouter.js
@@ -1,27 +1,44 @@
-import React from 'react';
-import {
-    BrowserRouter as Router,
-    Redirect,
-    Route,
-    Switch,
-} from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Redirect, Switch } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import { LoginScreen } from '../components/auth/LoginScreen';
 import { CalendarScreen } from '../components/calendar/CalendarScreen';
+import { startChecking } from '../actions/auth';
+import { PublicRoute } from './PublicRoute';
+import { PrivateRoute } from './PrivateRoute';
+
 /**
  * exact /login -LoginScreen
  * exact / -CalendarScreen
  */
 export const AppRouter = () => {
+    const dispatch = useDispatch();
+    const { checking, uid } = useSelector((state) => state.auth);
+
+    useEffect(() => {
+        dispatch(startChecking());
+    }, [dispatch]);
+
+    if (checking) {
+        return <h5>Wait...</h5>;
+    }
+
     return (
         <Router>
             <div>
                 <Switch>
-                    <Route exact path="/login">
-                        <LoginScreen />
-                    </Route>
-                    <Route path="/">
-                        <CalendarScreen />
-                    </Route>
+                    <PublicRoute
+                        exact
+                        path="/login"
+                        component={LoginScreen}
+                        isAuthenticated={!!uid}
+                    />
+                    <PrivateRoute
+                        exact
+                        path="/"
+                        component={CalendarScreen}
+                        isAuthenticated={!!uid}
+                    />
                     <Redirect to="/" />
                 </Switch>
             </div>
--- 09-calendar-app/src/types/types.js
+++ 09-calendar-app/src/types/types.js
@@ -7,4 +7,10 @@ export const types = {
     eventClearActiveEvent: '[event] Clear Active Event',
     eventUpdated: '[event] Event Updated',
     eventDeleted: '[event] Event Deleted',
+
+    authCheckingFinish: '[auth] Finish checking login state',
+    authStartLogin: '[auth] Start login',
+    authStartRegister: '[auth] Start register',
+    authStartTokenRenew: '[auth] Start token renew',
+    authLogout: '[auth] Logout',
 };
