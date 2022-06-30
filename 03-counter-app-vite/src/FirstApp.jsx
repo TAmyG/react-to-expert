a6bb136ed3364d83b727eaf1cae79799d35bab3e
vn53peo <tamy.vivas@walmart.com>
2022-06-29 21:59:49 -0600

    feat: introduction v18

---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const FirstApp = ({title= "Hola a todos"}) => {
+  return (
+    <div>FirstApp: {props.title}</div>
+  )
+}
---
+++ 03-counter-app-vite/tests/demo.test.js
@@ -0,0 +1,14 @@
+
+describe('first Test DEMO', () => { 
+    
+    test('should first', () => { 
+    
+        const message1  = 'Hola Mundo';
+    
+        const message2 = message1.trim();
+    
+        expect(message1).toBe(message2);
+        
+    });
+ })
+
