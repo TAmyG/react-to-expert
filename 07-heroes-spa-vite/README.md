fc501edb234f1d89374f3a53afd7b193be5fda1a
vn53peo <tamy.vivas@walmart.com>
2022-07-22 11:31:56 -0600

    Testing setup

--- 07-heroes-spa-vite/README.md
+++ 07-heroes-spa-vite/README.md
@@ -5,3 +5,12 @@ npm add react-router-dom@6
 https://animate.style/
 
 npm add query-string
+
+Testing
+guia https://gist.github.com/Klerith/ca7e57fae3c9ab92ad08baadc6c26177
+
+npm add --dev jest babel-jest @babel/preset-env @babel/preset-react
+npm add --dev @testing-library/react @types/jest jest-environment-jsdom
+
+opcional para node < 18 y si utilizamos el fetch
+npm add --dev whatwg-fetch
--- 07-heroes-spa-vite/src/auth/context/authReducer.js
+++ 07-heroes-spa-vite/src/auth/context/authReducer.js
@@ -1,16 +1,16 @@
-import { types } from "../types/types";
+import { types } from '../types/types';
 
 // const initialState = {
 //     logged: false,
 // }
 
-export const authReducer = (state =  {}, action) => {
+export const authReducer = (state = {}, action) => {
     switch (action.type) {
         case types.login:
             return {
                 ...state,
                 logged: true,
-                user: action.payload
+                user: action.payload,
             };
 
         case types.logout:
@@ -20,6 +20,6 @@ export const authReducer = (state =  {}, action) => {
                 user: null,
             };
         default:
-            state;
+            return state;
     }
-}
+};
